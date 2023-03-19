9610c36edd32dde16c4354ca8a9cd869062e4596
vishutrickcoder <vs1015242@gmail.com>
2023-03-19 13:06:42 +0530

    Using some javaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+console.log(document.querySelector('.message').textContent)
+
+document.querySelector('.message').textContent = " 🍾 Correct Answer!!!"
+document.querySelector('.number').textContent = 17
+document.querySelector('.score').textContent = 12
+document.querySelector('.guess').value = 29;
+console.log(document.querySelector('.guess').value )
