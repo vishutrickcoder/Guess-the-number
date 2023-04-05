48a8b5c364d325304c663a69b9eb51234b403198
vishutrickcoder <vs1015242@gmail.com>
2023-04-05 23:44:46 +0530

    Complete

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ let SecretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
 let highScore = 0;
 
+
 document.querySelector(".check").addEventListener("click" , function(){
     const guess = Number(document.querySelector(".guess").value);
     console.log( guess ,typeof guess)
