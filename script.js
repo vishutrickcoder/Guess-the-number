02f65537f26317becddd1be1b0871cc5df3b6066
vishutrickcoder <vs1015242@gmail.com>
2023-04-05 23:33:51 +0530

    add functionality

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ document.querySelector('.guess').value = 29;
 console.log(document.querySelector('.guess').value )
 */
 
-const SecretNumber = Math.trunc(Math.random()*20)+1;
+let SecretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
-document.querySelector(".number").textContent = SecretNumber;
+let highScore = 0;
 
 
 
@@ -21,8 +21,20 @@ document.querySelector(".check").addEventListener("click" , function(){
     
     if(!guess){
         document.querySelector(".message").textContent = "⛔️ No Number!!!";
+
     }else if(guess === SecretNumber){
+
         document.querySelector(".message").textContent = " 🍾 Correct Answer!!!";
+        document.querySelector(".number").textContent = SecretNumber;
+        document.querySelector("body").style.backgroundColor = "#60b347"
+        document.querySelector(".number").style.width = "30rem"
+
+        if(score > highScore){
+            highScore = score;
+            document.querySelector(".highscore").textContent = highScore 
+
+        }
+
     }else if(guess > SecretNumber){
         if(score > 1 ){
             document.querySelector(".message").textContent = "📈 Too High!!";
@@ -44,4 +56,24 @@ document.querySelector(".check").addEventListener("click" , function(){
         }
        
     }
+});
+
+
+document.querySelector(".again").addEventListener("click" , function(){
+    score = 20;
+    SecretNumber = Math.trunc(Math.random()*20)+1;
+
+
+    document.querySelector('.message').textContent = "Start Guessing ....";
+    document.querySelector('.score').textContent = score;
+
+    document.querySelector(".number").textContent = "?";
+
+    document.querySelector('.guess').value  = "";
+
+    document.querySelector("body").style.backgroundColor = "#222"
+    document.querySelector(".number").style.width = "15rem"
+
+
+
 });
